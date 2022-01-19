bcbfa9f1a64e49d27e00842adfc1741cce3c6af6
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-01-19 13:05:39 +0100

    commit

--- src/components/Home/blog-home/HotPosts.tsx
+++ src/components/Home/blog-home/HotPosts.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Badge, Image } from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 import { Link } from "react-router-dom"
@@ -14,6 +14,7 @@ const HotPosts = () => {
 
     const dispatch = useDispatch()
     const posts = useSelector((state: ReduxState) => state.posts)
+    const [seeMore, setSeeMore] = useState(false)
 
    
     const newPost = posts.map(p => p).sort((a,b) => b.likes.length - a.likes.length)
@@ -40,12 +41,16 @@ const HotPosts = () => {
       useEffect(() => {
         getData()
       }, [length.length])
+
+      const toggle = () => {
+          seeMore === false ? setSeeMore(true) : setSeeMore(false)
+      }
     
     return (
         <div id='hotposts'>
             <h4 className="text-muted"># Top Posts</h4>
             { newPost && newPost ?
-                newPost.map((p, i) => (
+                newPost.slice(0, 5).map((p, i) => (
                 <Link key={i}  className="text-decoration-none text-dark" to={`/posts/${p._id}`}>
                     <div key={i} className="d-flex hotpostList mb-2">
                         <div> <Image roundedCircle src={p.cover} alt="" width="37" height="37" /></div>
@@ -58,9 +63,32 @@ const HotPosts = () => {
                         </Badge>
                         </div>
                     </div>  
-                </Link>  
+                </Link> 
                 )) 
                 : ( <Loader /> )
+            }
+            { seeMore === true ?
+            newPost.slice(5, 10).map((p, i) => (
+                <Link key={i}  className="text-decoration-none text-dark" to={`/posts/${p._id}`}>
+                    <div key={i} className="d-flex hotpostList mb-2">
+                        <div> <Image roundedCircle src={p.cover} alt="" width="37" height="37" /></div>
+                        <div className="mb-2 ml-3 text">
+                            <p className="strong">{p.text}</p>
+                        </div>
+                        <div className="ml-auto">
+                        <Badge pill variant="secondary">
+                            {p.likes.length}
+                        </Badge>
+                        </div>
+                    </div>  
+                </Link>)) : null
+            }
+            { seeMore === false ?
+                <p className="text-center p-0 seeMore"
+                onClick={() => toggle()}>See More</p>
+                :
+                <p className="text-center p-0 seeMore"
+                onClick={() => toggle()}>See Less</p> 
             }
         </div>
     ) 
