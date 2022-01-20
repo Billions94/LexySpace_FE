9f32dc638c7625422869974c0b543ca5ea67e2ac
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-01-20 17:45:45 +0100

    commit

--- src/components/Home/profile/recentPost/RecentItem.tsx
+++ src/components/Home/profile/recentPost/RecentItem.tsx
@@ -1,4 +1,5 @@
 import { Posts } from '../../../../redux/interfaces'
+import { useNavigate } from 'react-router-dom'
 
 
 interface RecentItemProps {
@@ -6,9 +7,13 @@ interface RecentItemProps {
 }
 
 const RecentItem = ({ post }: RecentItemProps) => {
+
+    const navigate = useNavigate()
+
     return (
         <>
-            <div className="postContainer ml-3">
+            <div className="postContainer ml-3"
+                onClick={() => navigate(`/posts/${post._id}`)}>
                 <img src={post.cover} alt='' width={45} height={45} />
                 <div>
                     {post.text}
